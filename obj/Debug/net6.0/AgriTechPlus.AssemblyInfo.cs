6f5336118e278eeb176f8a2a85daaa80d9276e04
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2023-04-19 18:59:26 -0300

    Init

--- obj/Debug/net6.0/AgriTechPlus.AssemblyInfo.cs
+++ obj/Debug/net6.0/AgriTechPlus.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AgriTech2.0")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AgriTechPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("AgriTech2.0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AgriTech2.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("AgriTechPlus")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AgriTechPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
